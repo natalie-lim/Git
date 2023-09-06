a824c916c6b39f1d37cb3c7cdcc489f5eedbad60
WilliamAbraham <williamjohnabraham1@gmail.com>
2023-09-06 10:26:22 -0700

    added compression, but not sure if it's correct one...

--- blob.java
+++ blob.java
@@ -6,20 +6,36 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
+import java.io.ByteArrayOutputStream;
+import java.util.zip.*;
+
 
 public class Blob{
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         Blob blob = new Blob("test.txt");
     }
 
     //Takes a String Filename and reads its contents
     //Hash the content to another String variable
     //Writes content to a fileName with Hash to Objects
-    public Blob(String fileName){
+    public Blob(String fileName) throws IOException{
         File obj = new File(fileName);
         String content = read(obj);
-        String hashed = encryptThisString(content);
-        write(hashed, content);
+        String compressedContent = compress(content);
+        String hashed = encryptThisString(compressedContent);
+        write(hashed, compressedContent, "Objects");
+    }
+
+    public static String compress(String str) throws IOException{ 
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        GZIPOutputStream gzip = new GZIPOutputStream(out);
+        gzip.write(str.getBytes());
+        gzip.close();
+        return out.toString("ISO-8859-1");
     }
 
     //Hashes String
@@ -62,10 +78,10 @@ public class Blob{
     }
 
     //Writes fileName with content to Objects directory
-    public static void write(String fileName, String content){
+    public static void write(String fileName, String content, String directory){
         try
         {
-            File file = new File("Objects", fileName);
+            File file = new File(directory, fileName);
             FileWriter fw = new FileWriter(file);
             fw.write(content);
             fw.close();
@@ -75,4 +91,19 @@ public class Blob{
             e.printStackTrace();
         }
     }
+
+    public static void write(String fileName, String content){
+        try
+        {
+            File file = new File(fileName);
+            FileWriter fw = new FileWriter(file);
+            fw.write(content);
+            fw.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
 }
