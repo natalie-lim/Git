edf845e3043b21791a72a06d75ae1ae8fbfad788
Chase <102624440+uChase@users.noreply.github.com>
2023-09-13 14:40:23 -0700

    crazy

--- TreeTest.java
+++ TreeTest.java
@@ -46,6 +46,7 @@ public class TreeTest {
 
         tree.add("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976");
         tree.add("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
+        tree.add("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
         tree.add("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f : file1.txt");
         tree.add("blob : 01d82591292494afd1602d175e165f94992f6f5f : file2.txt");
         tree.remove("bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
